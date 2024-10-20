d97e540473f5c78c1f8fbb7bc9fe333b438e1846
Mohammed Salah Zourob <mohammed.dev.2017@gmail.com>
2024-10-20 09:21:22 +0300

    Update doctorItem.vue

--- src/components/doctors/doctorItem.vue
+++ src/components/doctors/doctorItem.vue
@@ -1,27 +1,29 @@
 <template>
-  <td>{{ id }}</td>
-  <td>{{ name }}</td>
-  <td>{{ department }}</td>
-  <td>{{ specialization }}</td>
-  <td>{{ degree }}</td>
-  <td>{{ mobile }}</td>
-  <td>{{ email }}</td>
-  <td>{{ joining_date }}</td>
+  <tr class="odd gradeX">
+    <td>{{ id }}</td>
+    <td>{{ name }}</td>
+    <td>{{ department }}</td>
+    <td>{{ specialization }}</td>
+    <td>{{ degree }}</td>
+    <td>{{ mobile }}</td>
+    <td>{{ email }}</td>
+    <td>{{ joining_date }}</td>
 
-  <td v-if="is_active == true">
-    <button type="button" class="btn btn-info">Active</button>
-  </td>
-  <td v-else>
-    <button type="button" class="btn btn-danger">UnAvtive</button>
-  </td>
-  <td>
-    <button class="btn btn-primary btn-xs">
-      <font-awesome-icon :icon="['fas', 'pencil']" />
-    </button>
-    <button class="btn btn-danger btn-xs">
-      <font-awesome-icon :icon="['fas', 'trash-can']" />
-    </button>
-  </td>
+    <td>
+      <button class="btn btn-info" @click="toggleValue">
+        {{ is_active ? "Active" : "Inactive" }}
+      </button>
+    </td>
+
+    <td>
+      <button class="btn btn-primary btn-xs">
+        <font-awesome-icon :icon="['fas', 'pencil']" />
+      </button>
+      <button class="btn btn-danger btn-xs">
+        <font-awesome-icon :icon="['fas', 'trash-can']" />
+      </button>
+    </td>
+  </tr>
 </template>
 
 <script>
@@ -37,5 +39,17 @@ export default {
     "joining_date",
     "is_active",
   ],
+  computed: {
+    toggleValue() {
+      if (this.is_active == "1") {
+        this.is_active = !this.is_active;
+      }
+      console.log(this.is_active);
+      // const i = (id = this.id);
+      // this.$store.getters["isDoctor"];
+      // console.log(this.$store.dispatch("changeStatus"));
+      // return this.$store.dispatch("changeStatus");
+    },
+  },
 };
 </script>
